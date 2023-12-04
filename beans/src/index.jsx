54494aaaa0ddbfeba63a4fb9868e50ab337d163f
westgateboy88 <133793148+westgateboy88@users.noreply.github.com>
2023-12-04 10:49:40 -0500

    app

--- beans/src/index.jsx
+++ beans/src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import home from './index.html'
-import reportWebVitals from './reportWebVitals';
+import home from './index.jsx'
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
